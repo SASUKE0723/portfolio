b1004de0ddcc44f8c173351bc0d7f4d64bf1fe1c
sasuke0723 <ryogo5.0304@gmail.com>
2022-12-04 10:59:02 +0000

    first

---
+++ public/assets/js/sketch.js
@@ -0,0 +1,28 @@
+var sketch;
+
+// 全体の初期化（最初に一回だけ呼ばれる）
+function setup() {
+    // キャンバスをつくる
+    createCanvas(windowWidth, windowHeight);
+    
+    // 背景をぬりつぶす
+    background(0);
+    
+    // ブレンドモードの設定
+    blendMode(ADD);
+
+}
+
+// 計算と描画
+function draw() {
+    // 輪郭（りんかく）を消す
+    noStroke();
+    // ぬりつぶす
+    fill(random(80), random(80), 255, 50);
+    
+    // 円を描く
+    ellipse( random(windowWidth),  random(windowHeight), random(5, 50));
+}
+
+
+new p5(sketch, "top");
